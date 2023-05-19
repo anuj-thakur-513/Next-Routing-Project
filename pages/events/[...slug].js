0c85963006a8ecbba9871beeec9da2b2f23c0fdc
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2023-05-19 20:17:44 +0530

    initial commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,54 @@
+import EventList from "@/components/events/event-list";
+import ResultsTitle from "@/components/events/results-title";
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <p className="center">Invalid Filter!!! Please adjust your values</p>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No events found for the chosen filter!</p>;
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+};
+
+export default FilteredEventsPage;
